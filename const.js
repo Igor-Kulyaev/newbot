ca4790cef413146f89796a24e45a2cc4981d5c71
Igor-Kulyaev <90397740+Igor-Kulyaev@users.noreply.github.com>
2021-09-15 17:24:16 +0300

    first commit

---
+++ const.js
@@ -0,0 +1,20 @@
+const commands = `
+/start - Start communication
+/help - Help
+`
+const text1 = `
+text for checking the processing and <a href="https://youtube.com/">link</a>
+`
+
+const text2 = `
+text for checking the processing and <a href="https://youtube.com/">link</a>
+`
+
+const text3 = `
+text for checking the processing and <a href="https://youtube.com/">link</a>
+`
+
+module.exports.commands = commands;
+module.exports.text1 = text1;
+module.exports.text2 = text2;
+module.exports.text3 = text3;
